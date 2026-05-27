cd0909a4246e56c4c9e96d2c060676d8264aa3d7
gianca04 <grancarlosavalogallardo@gmail.com>
2026-05-27 14:45:07 -0500

    Muerte de proceso en caso error critico.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import logging
 from config import AppConfig
 from gateway import IIoTGateway
@@ -11,6 +12,7 @@ def main():
         gateway.start()
     except Exception as e:
         logger.critical(f"Fallo crítico al iniciar el Edge Gateway IIoT: {e}", exc_info=True)
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
